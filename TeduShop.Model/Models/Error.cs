7c5409390027ff170a5294ec0799ac4f5cb43119
DESKTOP-FT76O01\adobe <hanlam809@gmail.com>
2016-12-29 00:52:42 +0700

    add web API

---
+++ TeduShop.Model/Models/Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Error")]
+    public class Error
+    {
+        [Key]
+        public int ID { set; get; }
+
+        public string Message { set; get; }
+        public string StackTrace { set; get; }
+        public DateTime CreatedDate { set; get; }
+    }
+}
