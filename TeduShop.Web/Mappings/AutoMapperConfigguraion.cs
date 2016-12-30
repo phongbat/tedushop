bceef8c334c51a012704437312614c344fdb6ea7
DESKTOP-FT76O01\adobe <hanlam809@gmail.com>
2016-12-30 20:36:02 +0700

    auttomapper for web

---
+++ TeduShop.Web/Mappings/AutoMapperConfigguraion.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostViewModel>().MaxDepth(2);
+                cfg.CreateMap<Tag, TagViewModel>().MaxDepth(2);
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>().MaxDepth(2);
+                cfg.CreateMap<PostTag, PostTagViewModel>().MaxDepth(2);
+            });
+        }
+    }
+}
